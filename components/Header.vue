b5aa59553b5182cf6395178e8de25160292a1e3d
RamaFajr <ramafajr9123@gmail.com>
2023-12-02 14:25:04 +0700

    update

--- components/Header.vue
+++ components/Header.vue
@@ -23,9 +23,7 @@ const logout = async () => {
 
 <template>
   <!-- Main navigation container -->
-  <nav
-    class="flex-no-wrap relative flex w-full items-center justify-between bg-[#FBFBFB] py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4"
-  >
+  <nav class="flex-no-wrap relative flex w-full items-center justify-between bg-[#FBFBFB] py-2 shadow-md shadow-black/5 dark:bg-neutral-600 dark:shadow-black/10 lg:flex-wrap lg:justify-start lg:py-4">
     <div class="flex w-full flex-wrap items-center justify-between px-3">
       <!-- Hamburger button for mobile view -->
       <button
@@ -39,12 +37,7 @@ const logout = async () => {
       >
         <!-- Hamburger icon -->
         <span class="[&>svg]:w-7">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="h-7 w-7"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-7 w-7">
             <path
               fill-rule="evenodd"
               d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
@@ -55,23 +48,11 @@ const logout = async () => {
       </button>
 
       <!-- Collapsible navigation container -->
-      <div
-        class="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
-        id="navbarSupportedContent12"
-        data-te-collapse-item
-      >
+      <div class="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto" id="navbarSupportedContent12" data-te-collapse-item>
         <!-- Logo -->
-        <img
-          src="../public/images/icons/img.png"
-          style="height: 50px"
-          alt="TE Logo"
-          loading="lazy"
-        />
+        <img src="../public/images/icons/icon.png" style="height: 50px" alt="TE Logo" loading="lazy" />
         <!-- Left navigation links -->
-        <ul
-          class="list-style-none mr-auto flex flex-col pl-0 lg:flex-row"
-          data-te-navbar-nav-ref
-        >
+        <ul class="list-style-none mr-auto flex flex-col pl-0 lg:flex-row" data-te-navbar-nav-ref>
           <li class="mb-4 lg:mb-0 lg:pr-2 mt-2" data-te-nav-item-ref>
             <nuxt-link
               to="/"
@@ -96,26 +77,11 @@ const logout = async () => {
             >
           </li> -->
           <li class="mb-4 lg:mb-0 lg:pr-2 mt-2" data-te-nav-item-ref>
-            <NuxtLink
-              to="/login"
-              class="text-base bg-blue-600 px-6 py-2 text-white rounded-lg hover:bg-blue-600/80"
-              >Login</NuxtLink
-            >
+            <NuxtLink to="/login" class="text-base bg-blue-600 px-6 py-2 text-white rounded-lg hover:bg-blue-600/80">Login</NuxtLink>
           </li>
           <li class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-            <NuxtLink
-              v-if="!isAuthenticated"
-              to="/login"
-              class="text-base bg-blue-600 px-6 py-2 text-white rounded-lg hover:bg-blue-600/80"
-              >Login</NuxtLink
-            >
-            <div
-              v-else
-              class="text-base cursor-pointer bg-red-600 px-6 py-2 text-white rounded-lg hover:bg-red-600/80"
-              @click="logout"
-            >
-              Logout
-            </div>
+            <NuxtLink v-if="!isAuthenticated" to="/login" class="text-base bg-blue-600 px-6 py-2 text-white rounded-lg hover:bg-blue-600/80">Login</NuxtLink>
+            <div v-else class="text-base cursor-pointer bg-red-600 px-6 py-2 text-white rounded-lg hover:bg-red-600/80" @click="logout">Logout</div>
           </li>
         </ul>
       </div>
